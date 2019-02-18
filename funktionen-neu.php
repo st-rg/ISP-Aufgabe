292e7e18194562d973bb577c4fcff6ca83a7b0e1
Steffi Reeg <stefanie.reeg@gmail.com>
2019-02-18 15:16:41 +0100

    Kommentare hinzugefügt, RegEx überarbeitet

--- funktionen-neu.php
+++ funktionen-neu.php
@@ -1,19 +1,28 @@
 <?php
 /**
  * Created by PhpStorm.
+ *
+ * In dieser Datei befinden sich die Funktionen, die für die Erzeugung der Eingabefelder benötigt werden,
+ * außerdem die Funktionen, die die Eingaben überprüfen und die HTML-Seiten erzeugen
+ *
+ * @author Steffi Reeg <s50213@beuth-hochschule.de>
  * User: stefanie_reeg
  * Date: 2019-02-05
  * Time: 10:19
  */
 
-//funktion erstellt ein Eingabefeld
+/* Die Funktion erstellt ein Eingabefeld, bekommt als Wert einen String mit dem gewünschten
+* Namen des Eingabefeldes
+*/
 function erstelleEingabefeld($nameEingabefeld) {
 
     echo $nameEingabefeld.': '.'<input type="text" name="'.$nameEingabefeld. '"/>'.'<br>';
 
 }
 
-
+/* funktion erstellt ein Eingabefeld mit rotem Text und einem Hinweistext, der auch rot dargestellt wird
+* der Hinweistext informiert, dass Sonderzeichen nicht erlaubt sind
+*/
 function erstelleEingabefeldrot($nameEingabefeld) {
 
     echo '<div class="red">'.$nameEingabefeld.': '.'</div>'.'<input type="text" name="'.$nameEingabefeld. '"/>'.'<br>';
@@ -21,25 +30,20 @@ function erstelleEingabefeldrot($nameEingabefeld) {
 }
 
 
-//funktion erstellt ein Button
+/* funktion erstellt ein Button, bekommt als Werte den Typ des Buttons, den gewünschten Namen des Buttons
+* und den Wert, der im Button stehen soll
+*/
 function erstelleInput($inputType, $inputName, $inputWert) {
 
     echo '<input type="'.$inputType.'" name="'.$inputName.'" value="'.$inputWert.'" class="button float rund"/>';
 }
 
 
-function pruefeName($name) {
-
-    if($name == 'steffi reeg') {
-        return true;
-    } else {
-        return false;
-    }
-
-}
-
-
-
+/* Die Funktion bekommt die Daten aus dem Array $_POST und prüft, ob die Daten unzulässige Sonderzeichen enthalten.
+* Sind die Felder gar nicht ausgefüllt, wird das Formular einfach noch einmal dargestellt.
+* Sind alle Daten korrekt, wird die zweite Seite der Website dargestellt. Sind Fehler in den Daten, wird erneut
+* die erste Seite der Website dargestellt, mit roten Warnhinweisen.
+*/
 function pruefeReiseziele($eingabe) {
 
     $reisezieleNichtOK = [];
@@ -52,49 +56,62 @@ function pruefeReiseziele($eingabe) {
     $falscheEingabe = 0;
     $korrekteEingabe = 0;
 
-    $sonderzeichen = '/[^0-9a-zA-ZöüäÖÜÄ]/';
+    /* Beginnt am Anfang des Strings und sucht bis zum Ende des Strings.
+    * Erlaubt sind alle alphanumerischen Zeichen, Leerzeichen, Bindestrich und Punkt.
+    * Leider kann man jetzt Leerzeichen, Bindestrich und Punkt mehrmals hintereinander eingeben,
+    * Man kann Schriftzeichen aus allen Sprachen verwenden.
+    */
+    $sonderzeichen = '/^[\w\s\-\.]+$/';
 
 
    foreach($eingabe as $schluessel=>$wert) {
+       //alle Daten des Arrays werden geprüft, außer dem versteckten Button mit dem Namen 'seite-1'
+       if($schluessel !== 'seite-1') {
 
 
+           if ($wert == '') {
 
-              if ($wert == '') {
+               $reisezieleNichtOK[$zaehler] = '<label for="Urlaubsziel' . $nummer . '">' . 'Urlaubsziel ' . $nummer . ': </label>' . '<input type="text" name="' . 'Urlaubsziel ' . $nummer . '"/>' . '<br>';
+               $reisezieleOK[$zaehler] = '<p class="uppercase">Urlaubziel' . $nummer . ': ' . $wert . '</p><br>';
+               $leeresFeld++;
+               $zaehler++;
+               $nummer++;
 
-                  $reisezieleNichtOK[$zaehler] = '<label for="Urlaubsziel'.$nummer.'">'.'Urlaubsziel '.$nummer.': </label>'.'<input type="text" name="'.'Urlaubsziel '.$nummer.'"/>'.'<br>';
-                  $reisezieleOK[$zaehler] = '<p class="uppercase">Urlaubziel'.$nummer.': '.$wert.'</p><br>';
-                  $leeresFeld++;
-                  $zaehler++;
-                  $nummer++;
-              }
+           } else if (!preg_match($sonderzeichen, $wert)) {
 
-              else if (preg_match($sonderzeichen, $wert)) {
+               $reisezieleNichtOK[$zaehler] = '<label for="Urlaubsziel ' . $nummer . '"class="rot">' . 'Urlaubsziel ' . $nummer . '</label>' . '<input type="text" name="' . 'Urlaubsziel ' . $nummer . '" value="' . $wert . '"/>' . '<br>' .
+                   '<p class="rot">Sonderzeichen sind nicht erlaubt!!!</p>';
+               $falscheEingabe++;
+               $zaehler++;
+               $nummer++;
 
-                  $reisezieleNichtOK[$zaehler] = '<label for="Urlaubsziel '.$nummer.'"class="rot">'.'Urlaubsziel '.$nummer.'</label>'.'<input type="text" name="'.'Urlaubsziel '.$nummer.'" value="'.$wert.'"/>'.'<br>'.
-                  '<p class="rot">Sonderzeichen sind nicht erlaubt!!!</p>';
-                  $falscheEingabe++;
-                  $zaehler++;
-                  $nummer++;
+           } else {
 
+               $reisezieleNichtOK[$zaehler] = 'Urlaubsziel ' . $nummer . ': ' . $wert . '<br>';
+               $reisezieleOK[$zaehler] = '<p class="uppercase">Urlaubziel ' . $nummer . ': ' . $wert . '</p><br>';
+               $korrekteEingabe++;
+               $zaehler++;
+               $nummer++;
 
-            } else {
+           }
 
-                  $reisezieleNichtOK[$zaehler] = 'Urlaubsziel '.$nummer.': '.$wert.'<br>';
-                  $reisezieleOK[$zaehler] = '<p class="uppercase">Urlaubziel '.$nummer.': '.$wert.'</p><br>';
-                  $korrekteEingabe++;
-                  $zaehler++;
-                  $nummer++;
-
-              }
+       }
    }
 
    if($falscheEingabe>0) {
        erstelleFormularSeite1($reisezieleNichtOK);
    }
 
+   //alle Bedingungen erfüllt
    else if ($falscheEingabe == 0 && $korrekteEingabe>0) {
-       file_put_contents('reisezieleOK.txt',$reisezieleOK);
+
+       //Daten lokal speichern
+       file_put_contents('daten.txt',$reisezieleOK);
+
+       //Array initialisieren, das die Eingabefelder für die zweite Seite enthält
        $kontaktdaten = ['<label for="name">Name:</label>'.'<input type="text" name="name"/>'.'<br>', '<label for="adresse">Adresse:</label>'.'<input type="text" name="adresse"/>'.'<br>', '<label for=telefon">Telefon: </label>'.'<input type="text" name="telefon"/>'.'<br>'];
+
+       //Funktion aufrufen, die die zweite Seite erstellt
        erstelleFormularSeite2($kontaktdaten);
    }
 
@@ -105,6 +122,9 @@ function pruefeReiseziele($eingabe) {
 }
 
 
+/* die Funktion bekommt die Daten aus dem Array $_POST und prüft, ob die Felder ausgefüllt sind und ob die eingegebenen Daten
+* korrekt sind
+*/
 function pruefeKontaktdaten($eingabe) {
 
     $fehlerGefunden = 0;
@@ -112,9 +132,29 @@ function pruefeKontaktdaten($eingabe) {
     $kontaktdatenOK = [];
     $kontaktdatenNichtOK = [];
 
-    $pruefeName = '/^[A-ZÄÖÜ]{1}[a-zäüö]+(\-|\s)[A-ZÄÜÖ]{1}[a-zäüö]+\s[A-ZÄÜÖ]{1}[a-zöüä]+(\-|\s)[A-ZÄÜÖ]{1}[a-zäöü]+/';
-    $pruefeAdresse = '/ /';
-    $pruefeTelefon = '/ /';
+    /* Beginnt am Anfang des Strings und sucht bis zum Ende des Strings.
+   * Erlaubt sind alle alphanumerischen Zeichen, Leerzeichen, Bindestrich und Punkt.
+   * Leider kann man jetzt Leerzeichen, Bindestrich und Punkt mehrmals hintereinander eingeben,
+   * Man kann Schriftzeichen aus allen Sprachen verwenden.
+   */
+    $pruefeName = '/^[\w\s\-\.]+$/';
+
+
+    /* Beginnt am Anfang des Strings und sucht bis zum Ende des Strings.
+    * Erlaubt sind alle alphanumerischen Zeichen, Leerzeichen, Bindestrich und Punkt.
+   * Leider kann man jetzt Leerzeichen, Bindestrich und Punkt mehrmals hintereinander eingeben,
+   * Man kann Schriftzeichen aus allen Sprachen verwenden.
+     * Am Ende ist noch ein Zusatz möglich, z. B. Müllerstraße 7b. Erlaubt sind Zeichen von A-Z, groß- oder
+     * kleingeschrieben.
+     */
+    $pruefeAdresse = '/^[\w\-\s\.]+[a-zA-Z]?$/';
+
+
+    /* Erlaubt sind Ziffern, Plus, Punkt, Klammern und Leerzeichen. Auch Schrägstriche und Bindestriche sind erlaubt.
+     * So kann man verschiedene Schreibweisen von Telefonnummern eingeben.
+     */
+    $pruefeTelefon = '/\+?\(?[0-9]+\.?\s?\(?\)?\-?/';
+
 
 
     foreach ($eingabe as $schluessel=>$wert) {
@@ -122,17 +162,51 @@ function pruefeKontaktdaten($eingabe) {
         if($schluessel == 'name') {
 
                 if($wert !== '' && preg_match($pruefeName, $wert)) {
-                    $kontaktdatenOK[$zaehler] = '<p class="uppercase">Name: '.$wert.'</p><br>';
-                    $kontaktdatenNichtOK[$zaehler] = '<p class="uppercase">Name: '.$wert.'</p><br>';
+                    $kontaktdatenOK[$zaehler] = '<p class="uppercase">Name: '.$wert.'</p>'.'<br>';
+                    $kontaktdatenNichtOK[$zaehler] = '<label for "name">Name:</label><input type="text" name="name" value="'.$wert.'" />'.'<br>';
                     $zaehler++;
                 } else {
-                    $kontaktdatenNichtOK[$zaehler] = '<label for"name" class="rot">Falscher Name:</label><input type="text" name="name" />'.'<br>'
+                    $kontaktdatenNichtOK[$zaehler] = '<label for"name" class="rot">Name:</label><input type="text" name="name" />'.'<br>'
                                                      .'<p class="rot">Bitte geben Sie einen korrekten Namen ein!</p>';
+                    $zaehler++;
                     $fehlerGefunden++;
                 }
         }
 
+        if($schluessel == 'adresse') {
+
+                if($wert !== '' && preg_match($pruefeAdresse, $wert)) {
+                    $kontaktdatenOK[$zaehler] = '<p class="uppercase">Adresse: '.$wert.'</p>'.'<br>';
+                    $kontaktdatenNichtOK[$zaehler] = '<label for "adresse">Adresse:</label><input type="text" name="adresse" value="'.$wert.'" />'.'<br>';
+                    $zaehler++;
+                } else {
+                    $kontaktdatenNichtOK[$zaehler] = '<label for "adresse" class="rot">Adresse:</label><input type="text" name="adresse" />'.'<br>'
+                                                      .'<p class="rot">Bitte geben Sie eine gültige Adresse ein!</p>';
+                    $zaehler++;
+                    $fehlerGefunden++;
+                }
+
+
+        }
+
+        if($schluessel == 'telefon') {
+
+            if($wert !== '' && preg_match($pruefeTelefon, $wert)) {
+                $kontaktdatenOK[$zaehler] = '<p class="uppercase">Telefon: '.$wert.'</p>'.'<br>';
+                $kontaktdatenNichtOK[$zaehler] = '<label for "telefon">Telefon:</label><input type="text" name="telefon" value="'.$wert.'" />'.'<br>';
+                $zaehler++;
+            } else {
+                $kontaktdatenNichtOK[$zaehler] = '<label for "telefon" class="rot">Telefon:</label><input type="text" name="telefon" />'.'<br>'
+                    .'<p class="rot">Bitte geben Sie eine gültige Telefonnummer ein!</p>';
+                $zaehler++;
+                $fehlerGefunden++;
+            }
+
+
+        }
+
     }
+
 
     if($fehlerGefunden == 0) {
 
@@ -146,7 +220,7 @@ function pruefeKontaktdaten($eingabe) {
 }
 
 
-
+//stellt die erste Seite dar, bekommt Array mit Formularfeldern
 function erstelleFormularSeite1($felder) {
 
 
@@ -160,6 +234,7 @@ function erstelleFormularSeite1($felder) {
         echo $value;
     }
 
+    erstelleInput("hidden", "seite-1", "geklickt");
     erstelleInput("reset", "","reset");
     erstelleInput("submit", "","submit");
 
@@ -169,12 +244,15 @@ function erstelleFormularSeite1($felder) {
 
 }
 
-
+/* stellt die zweite Seite dar, liest Daten aus der Textdatei 'daten.txt' ein
+* bekommt ein Array Formularfeldern
+ *
+ */
 function erstelleFormularSeite2($daten) {
 
         echo '<h2 id="formular_headline">Kontaktdaten eingeben!</h2>';
 
-        print_r((file_get_contents('reisezieleOK.txt')));
+        print_r((file_get_contents('daten.txt')));
 
         echo '<form action="index.php" method="Post">';
 
@@ -193,15 +271,22 @@ function erstelleFormularSeite2($daten) {
 
 }
 
-function erstelleFormularSeite3($daten) {
+
+/* stellt die dritte Seite dar, bekommt ein Array mit Daten
+ * speichert die Daten in die Datei 'daten.txt', gibt den
+ * Inhalt der Datei auf der Seite aus
+ */
+function erstelleFormularSeite3($kontaktdaten) {
 
     echo '<h2 id="formular_headline">Wunschübersicht</h2>';
 
-    print_r(file_get_contents('reisezieleOK.txt'));
-
-    foreach($daten as $wert) {
-        echo $wert.'<br>';
-
+    $gespeicherteDaten = fopen("daten.txt", "a");
+    foreach($kontaktdaten as $value=>$item) {
+        fputs($gespeicherteDaten, $item);
     }
+
+    fclose($gespeicherteDaten);
+
+    print_r(file_get_contents('daten.txt'));
 
 }
--- index.php
+++ index.php
@@ -1,22 +1,50 @@
 <?php
 
+/**
  * Diese Datei legt einige Header für die Webseite fest, zum Beispiel den Etag-Header.
+ * Beim ersten Seitenaufruf stellt sie das Eingabeformular dar.
+ *
+ * @author Steffi Reeg <s50213@beuth-hochschule.de>
+
+ */
+
+// lädt die php-Datei mit den Funktionen hinzu
 require 'funktionen-neu.php';
 
+
+ /* diese eingebaute Funktion liefert die Angabe, wann die Datei zuletzt modifiziert wurde
+ * __FILE__ ist eine magische (vordefinierte) Konstante, die den vollständigen Pfad- und Dateinamen der Datei liefert
+*/
 $file_last_mod_time = filemtime(__FILE__);
+
+
+/* Hier habe ich jetzt einfach mal einen Wert eingetragen, weil ich gar nicht herausbekommen habe, wie man den Wert, wann
+ * der Inhalt zuletzt verändert wurde, herausbekommt
+*/
 $content_last_mod_time = 7;
+
+
+
+// Variable speichern, die später das unverwechselbare Etag für den Header wird
 $etag = '"'.$file_last_mod_time.'.'.$content_last_mod_time.'"';
+
 // fügt Header mit Datum und Uhrzeit mit der letzten Änderung des Dokuments hinzu
 echo header("Last-Modified: ".date("D, d M Y H:i:s")."GMT");
+
+
 /* fügt Header hinzu: Antworten vom Server müssen im Cache aufgehoben werden und können für
 * die nächsten 24 Stunden wiederverwendet werden
 */
 echo header("Cache-control: max-age=84600");
-/*
-* fügt Header mit eTag hinzu: ein unverwechselbares Merkmal für den Inhalt der Seite.
-*/
+
+
+
+// fügt Header mit eTag hinzu: ein unverwechselbares Merkmal für den Inhalt der Seite.
 echo header('ETag: '.$etag);
 
+
+
+// heredoc-Syntax gibt das Head-Tag für die Website aus
 echo <<<EOT
 <head> 
 <style type="text/css"> @import "./css/normalize.css";</style>
@@ -25,8 +53,8 @@ echo <<<EOT
 EOT;
 
 
-/**
- * wenn in der Variable $_SERVER das Feld 'HTTP_IF_NONE_MATCH' existiert und
+
+ /* wenn in der Variable $_SERVER das Feld 'HTTP_IF_NONE_MATCH' existiert und
  * identisch mit dem Etag der Anfrage ist, wird der Header der Antwort auf 304 Not Modified gesetzt,
  * die Daten haben sich nicht verändert und müssen nicht noch neu geschickt werden.
  * Die Seite kann aus dem Cache geladen werden.
@@ -39,8 +67,8 @@ if(isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
 }
 
 
-/**
- * diese Abfrage schaut in die Variable $_POST. Wenn dort noch keine Daten drin sind, bedeutet das, dass die Seite
+
+ /* diese Abfrage schaut in die Variable $_POST. Wenn dort noch keine Daten drin sind, bedeutet das, dass die Seite
  * zum ersten Mal aufgerufen wird. Es wird ein Array mit Html-Tags für die Formularfelder erzeugt und der Funktion
  * übergeben, die die Html-Seite mit dem Formular für die erste Seite erzeugt, übergeben.
  */
@@ -57,13 +85,13 @@ if (count($_POST) == 0) {
 }
 
 
-/** im Array $_POST sind schon Daten drin, vielleicht existiert sogar das Feld 'name' schon.
- * die Funktion, die die Daten prüft und bei Korrektheit die dritte Seite des Formulars darstellt,
- * wird aufgerufen
- * ist das Feld 'name' noch nicht vorhanden, aber im Array $_POST ist schon etwas drin, wird die Funktion, die diese Daten
- * überprüft und bei Korrektheit die zweite Seite des Formulars aufruft, ausgeführt
+/* wenn $_POST schon Daten enthält, muss herausgefunden werden, ob die zweite oder die dritte Seite der Website erzeugt werden
+ * soll. Auf der zweiten Seite gibt es einen versteckten Button mit dem Namen 'seite-1'. Wenn das Formular auf der zweiten Seiten geschickt wird,
+ * enthält $_POST ein Feld mit dem Namen des versteckten Buttons. Dann müssen die eingegeben Reiseziele geprüft werden.
+ * Ist das Feld 'seite-1' nicht vorhanden, aber im Array $_POST befinden sich Daten, wird die Funktion aufgerufen, die die
+ * Kontaktdaten prüft.
 */
-else if (!isset($_POST['name'])) {
+else if (isset($_POST['seite-1'])) {
     pruefeReiseziele($_POST);
 } else {
     pruefeKontaktdaten($_POST);
